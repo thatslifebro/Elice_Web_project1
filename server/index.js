39870f3f89b82f91f7f7318e7f1d870a7e358868
[AI6/2반]김성연 <sy9804071@gmail.com>
2023-01-31 15:23:05 +0900

    feat : mongodb connect

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-const express = require("express");
-
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get("/", (req, res) => {
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(
+    'mongodb+srv://6team:6team@cluster0.dpy7y0u.mongodb.net/?retryWrites=true&w=majority',
+  )
+  .then(console.log('db연결 성공'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
   console.log(req.body);
 });
 
 app.listen(3001, (req, res) => {
-  console.log("시작");
+  console.log('시작');
 });
