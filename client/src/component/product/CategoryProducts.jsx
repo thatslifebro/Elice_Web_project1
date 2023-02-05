7e7e80a8d5c8a88cbe0f22f54c7e451107e94cee
unknown <go1jamuae@gmail.com>
2023-02-05 13:07:23 +0900

    12page

--- client/src/component/product/CategoryProducts.jsx
+++ client/src/component/product/CategoryProducts.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, DropdownButton, Dropdown } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Form,
+  Col,
+  DropdownButton,
+  Dropdown,
+} from 'react-bootstrap';
 import axios from 'axios';
 import ProductCard from './ProductCard';
 
 function CategoryProducts() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [categoryId, setCategoryId] = useState('');
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_SERVER_ADDRESS}/api/categories`)
@@ -16,21 +24,56 @@ function CategoryProducts() {
       .catch((err) => {
         console.log(err);
       });
-  }, [categories]);
+  }, []);
 
+  const handleCategory = (event) => {
+    event.preventDefault();
+    setCategoryId(event.target.value);
+    console.log(categoryId);
+    console.log(event.currentTarget.value);
+  };
+
+  //카테고리Id로 물품정보를 모두 가져오는 로직 필요
+  //우선 물품정보 모두 가져와서 card로 보이기
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_ADDRESS}/api/products`)
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .then(console.log(products))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <Container fluid>
-      <Row xs="auto">
-        <DropdownButton id="dropdown-basic-button" title="카테고리">
-          {categories.map((category) => {
-            return (
-              <Dropdown.Item key={category._id}>{category.title}</Dropdown.Item>
-            );
-          })}
-        </DropdownButton>
-      </Row>
       <Row>
-        <ProductCard />;
+        <Col className="mb-2 ms-3 mr-5 ">
+          <Form>
+            <Form.Label htmlFor="SelectCategory">카테고리</Form.Label>
+            <Form.Select
+              className="mb-4"
+              id="SelectCategory"
+              aria-label="Default select example"
+              onChange={handleCategory}
+            >
+              <option>카테고리를 선택해주세요</option>
+              {categories.map((category) => {
+                return (
+                  <option key={category._id} value={category._id}>
+                    {category.title}
+                  </option>
+                );
+              })}
+            </Form.Select>
+          </Form>
+        </Col>
+      </Row>
+      <Row md={3} className="align-top align-left">
+        {products.map((product) => {
+          return <ProductCard key={product._id} product={product} />;
+        })}
       </Row>
     </Container>
   );
