3c034659b7a523dd083cf7b183c9f2c5619051e4
jeongjinLee <dlwjdwls0908.elice@gmail.com>
2023-02-06 16:59:06 +0900

    order router/services -jj

---
+++ server/services/users.js
@@ -0,0 +1,12 @@
+// const { User } = require('../db/model');
+
+// export class UserModel {
+
+//     static getAll() {
+//       return User.find().select('email address fullName role');
+//     }
+
+//     static getOneById(id) {
+//       return User.findById(id).select('email address fullName role');
+//     }
+//   }
