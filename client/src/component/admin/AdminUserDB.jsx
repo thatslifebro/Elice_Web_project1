3d066af40bc5c092fae8cb29c4af21ac3a058375
jsm00929 <jsm00929@naver.com>
2023-02-08 19:46:21 +0900

    fix

--- client/src/component/admin/AdminUserDB.jsx
+++ client/src/component/admin/AdminUserDB.jsx
@@ -1,23 +1,61 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import instance from '../../util/axios-setting';
+import { Table } from 'react-bootstrap';
 
 function AdminUserDB() {
-  const token = localStorage.getItem('jwt');
-  instance
-    .get(`/api/users`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch(() => console.log('error'));
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    instance
+      .get(`/api/users`)
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch(() => console.log('error'));
+  }, []);
+
+  const UserDB = ({ user }) => {
+    const [code, setCode] = useState('');
+    const [address1, setAddress1] = useState('');
+    const [address2, setAddress2] = useState('');
+
+    useEffect(() => {
+      if (user.address) {
+        setCode(user.address.postalCode);
+        setAddress1(user.address.address1);
+        setAddress2(user.address.address2);
+      }
+    }, []);
+
+    return (
+      <tr key={user._id}>
+        <td>{user._id}</td>
+        <td>{user.email}</td>
+        <td>{user.fullName}</td>
+        <td>
+          {code},{address1},{address2}
+        </td>
+      </tr>
+    );
+  };
 
   return (
     <div>
-      <h1>hello</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>email</th>
+            <th>name</th>
+            <th>address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return <UserDB user={user} />;
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 }
