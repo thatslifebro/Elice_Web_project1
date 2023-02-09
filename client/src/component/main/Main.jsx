1af8323642e45e963f33f8b30d110906ac20ba12
jeongjinLee <dlwjdwls0908.elice@gmail.com>
2023-02-09 12:06:28 +0900

    fix_main,cart

--- client/src/component/main/Main.jsx
+++ client/src/component/main/Main.jsx
@@ -3,6 +3,9 @@ import { Figure, Image } from 'react-bootstrap';
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
 import { NavLink } from 'react-router-dom';
 import MultipleItems from './multipleItems';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 import main1 from './mainImage/main1.jpg';
 import main2 from './mainImage/main2.jpg';
@@ -10,35 +13,200 @@ import main3 from './mainImage/main3.jpg';
 
 import './Main.css';
 function Main() {
+  const settings = {
+    className: 'center',
+    infinite: true,
+    centerPadding: '20px',
+    slidesToShow: 4,
+    swipeToSlide: true,
+    afterChange: function (index) {},
+  };
+  const settings2 = {
+    focusOnSelect: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+  };
+
   return (
     <>
-      <MDBCarousel showIndicators showControls fade>
-        <MDBCarouselItem
-          className="w-100 d-block mb-5"
-          height={500}
-          itemId={1}
-          src={main1}
-          alt="..."
-        ></MDBCarouselItem>
-
-        <MDBCarouselItem
-          className="w-100 d-block mb-5"
-          height={500}
-          itemId={2}
-          src={main2}
-          alt="..."
-        ></MDBCarouselItem>
-
-        <MDBCarouselItem
-          className="w-100 d-block mb-5"
-          height={500}
-          itemId={3}
-          src={main3}
-          alt="..."
-        ></MDBCarouselItem>
-      </MDBCarousel>
       <div className="container">
-        <h5>신상</h5>
+        <MDBCarousel showIndicators showControls fade>
+          <MDBCarouselItem
+            className="w-100 d-block mb-5"
+            height={500}
+            itemId={1}
+            src={main1}
+            alt="..."
+          ></MDBCarouselItem>
+
+          <MDBCarouselItem
+            className="w-100 d-block mb-5"
+            height={500}
+            itemId={2}
+            src={main2}
+            alt="..."
+          ></MDBCarouselItem>
+
+          <MDBCarouselItem
+            className="w-100 d-block mb-5"
+            height={500}
+            itemId={3}
+            src={main3}
+            alt="..."
+          ></MDBCarouselItem>
+        </MDBCarousel>
+      </div>
+      <div className="container mb-4">
+        <div>
+          <h4>신상</h4>
+          <Slider {...settings}>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h6>딸기</h6>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h6>딸기</h6>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h6>딸기</h6>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h6>딸기</h6>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h6>딸기</h6>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h6>딸기</h6>
+              <p>12,300원</p>
+            </div>
+          </Slider>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <div>
+          <h4>추천 아이템</h4>
+          <Slider {...settings2}>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h5>목살 150g</h5>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
+                  width="100%"
+                  height="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h5>목살 150g</h5>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h5>목살 150g</h5>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h5>목살 150g</h5>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h5>목살 150g</h5>
+              <p>12,300원</p>
+            </div>
+            <div>
+              <NavLink to={'/product'}>
+                <img
+                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
+                  width="100%"
+                  alt="Lights"
+                />
+              </NavLink>
+              <h5>목살 150g</h5>
+              <p>12,300원</p>
+            </div>
+          </Slider>
+        </div>
+      </div>
+      <div className="container">
+        <h5>최근 본 상품</h5>
         <div className="row">
           <div className="col-md-4">
             <NavLink to={'/product'}>
@@ -78,42 +246,6 @@ function Main() {
             <p>12,300원</p>
           </div>
         </div>
-      </div>
-      <div className="container">
-        <Figure>
-          <h5>이번달 인기 상품입니다</h5>
-          <Figure.Caption></Figure.Caption>
-
-          <Figure.Image
-            width={171}
-            height={180}
-            style={{
-              marginLeft: 50,
-            }}
-            alt="recommend"
-            src="https://picsum.photos/600/600/?random"
-          />
-
-          <Figure.Image
-            width={171}
-            height={180}
-            style={{
-              marginLeft: 30,
-            }}
-            alt="recommend"
-            src="https://picsum.photos/600/600/?random"
-          />
-
-          <Figure.Image
-            width={171}
-            height={180}
-            style={{
-              marginLeft: 30,
-            }}
-            alt="recommend"
-            src="https://picsum.photos/600/600/?random"
-          />
-        </Figure>
       </div>
     </>
   );
--- client/src/component/order/Order.jsx
+++ client/src/component/order/Order.jsx
@@ -15,7 +15,7 @@ import {
 } from 'react-bootstrap';
 import axios from 'axios';
 import DaumPostcode from 'react-daum-postcode';
-import Cart from './Cart';
+import Cart from './cart';
 import instance from '../../util/axios-setting';
 
 function Order() {
@@ -169,7 +169,15 @@ function Order() {
                                     />
                                   </PopupDom>
                                 )}
-                                <Button onClick={closePostCode}>닫기</Button>
+                                <Button
+                                  onClick={closePostCode}
+                                  className="mb-1"
+                                  variant="secondary"
+                                  size="sm"
+                                  type="button"
+                                >
+                                  닫기
+                                </Button>
                               </div>
                             </div>
                             <Row>
