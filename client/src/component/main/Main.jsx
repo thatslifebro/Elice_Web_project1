c8d96952cb8e2c145f856bcfce45976bbe2d0b6f
jeongjinLee <dlwjdwls0908.elice@gmail.com>
2023-02-09 17:06:16 +0900

    fix_main

--- client/src/component/main/Main.jsx
+++ client/src/component/main/Main.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Figure, Image } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
+
+import instance from '../../util/axios-setting';
 import { NavLink } from 'react-router-dom';
-import MultipleItems from './multipleItems';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import MainCard from './mainCarosel';
 
 import main1 from './mainImage/main1.jpg';
 import main2 from './mainImage/main2.jpg';
@@ -13,22 +14,32 @@ import main3 from './mainImage/main3.jpg';
 
 import './Main.css';
 function Main() {
+  const [products, setProducts] = useState([]);
+  const [products2, setProducts2] = useState([]);
+
+  useEffect(() => {
+    instance.get(`/api/products`).then((res) => {
+      setProducts(res.data.slice(0, 8));
+      setProducts2(res.data.slice(10, 20));
+    });
+  }, []);
+
   const settings = {
     className: 'center',
     infinite: true,
-    centerPadding: '20px',
+    centerPadding: '30px',
     slidesToShow: 4,
     swipeToSlide: true,
     afterChange: function (index) {},
   };
   const settings2 = {
-    focusOnSelect: true,
+    className: 'center',
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 500,
+    centerPadding: '30px',
+    slidesToShow: 5,
+    swipeToSlide: true,
+    afterChange: function (index) {},
   };
-
   return (
     <>
       <div className="container">
@@ -58,193 +69,24 @@ function Main() {
           ></MDBCarouselItem>
         </MDBCarousel>
       </div>
-      <div className="container mb-4">
+      <div className="container mb-5">
         <div>
           <h4>신상</h4>
           <Slider {...settings}>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h6>딸기</h6>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h6>딸기</h6>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h6>딸기</h6>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h6>딸기</h6>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h6>딸기</h6>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h6>딸기</h6>
-              <p>12,300원</p>
-            </div>
+            {products.map((product) => {
+              return <MainCard product={product} />;
+            })}
           </Slider>
         </div>
       </div>
       <div className="container mb-4">
         <div>
-          <h4>추천 아이템</h4>
+          <h4>추천 상품</h4>
           <Slider {...settings2}>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h5>목살 150g</h5>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
-                  width="100%"
-                  height="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h5>목살 150g</h5>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h5>목살 150g</h5>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h5>목살 150g</h5>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h5>목살 150g</h5>
-              <p>12,300원</p>
-            </div>
-            <div>
-              <NavLink to={'/product'}>
-                <img
-                  src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
-                  width="100%"
-                  alt="Lights"
-                />
-              </NavLink>
-              <h5>목살 150g</h5>
-              <p>12,300원</p>
-            </div>
+            {products2.map((product) => {
+              return <MainCard product={product} />;
+            })}
           </Slider>
-        </div>
-      </div>
-      <div className="container">
-        <h5>최근 본 상품</h5>
-        <div className="row">
-          <div className="col-md-4">
-            <NavLink to={'/product'}>
-              <img
-                src="https://www.pngmart.com/files/21/Strawberries-PNG-Isolated-Picture.png"
-                width="100%"
-                height="200"
-                alt="Lights"
-              />
-            </NavLink>
-
-            <h4>딸기</h4>
-            <p>12,300원</p>
-          </div>
-          <div className="col-md-4">
-            <NavLink to={'/product'}>
-              <img
-                src="https://www.pngplay.com/wp-content/uploads/1/Grapes-PNG-Photos.png"
-                width="100%"
-                height="200"
-                alt="Lights"
-              />
-            </NavLink>
-            <h4>포도</h4>
-            <p>22,300원</p>
-          </div>
-          <div className="col-md-4">
-            <NavLink to={'/product'}>
-              <img
-                src="https://meattam.com/web/product/tiny/20200604/0ca48a97e692f29867ec586f1f75dc72.png"
-                width="100%"
-                height="200"
-                alt="Lights"
-              />
-            </NavLink>
-            <h4>목살 150g</h4>
-            <p>12,300원</p>
-          </div>
         </div>
       </div>
     </>
